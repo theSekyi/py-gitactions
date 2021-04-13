d4b4358437764567085a4efdba26f32ded180189
theSekyi <thesekyi@users.noreply.github.com>
2021-04-13 09:58:35 +0000

    Automated changes

--- script.py
+++ script.py
@@ -3,9 +3,11 @@ from sklearn.datasets import load_iris
 
 data = load_iris()
 
+
 def load_dataset(data):
     labels = data.target_names
     df = pd.DataFrame(data, columns=labels)
     return df
+
 
 print(load_dataset(data))
